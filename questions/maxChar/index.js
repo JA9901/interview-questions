f9ec31d5bb864176bca7eb343588d3f4381fa85c
JA9901 <jareyes999@gmail.com>
2023-01-04 15:07:23 -0500

    4 and 5

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -6,8 +6,46 @@
  * maxCharacter('foo 111123') === '1'
  */
 
+
+// Brute force
+// const maxCharacter = (str) => {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = "";
+
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//         max = charMap[char];
+//         maxChar = char;
+//     }
+//   }
+//   return maxChar;
+// };
+
+// module.exports = maxCharacter;
+
+
+// Top Tech
 const maxCharacter = (str) => {
-  
+  const charMap = new Map();
+  let max = 0;
+  let maxChar = "";
+
+  for (const char of str) {
+    charMap.set(char, charMap.get(char) + 1 || 1)
+  }
+
+  for (const [char, count] of charMap) {
+    if (count > max) {
+        max = count;
+        maxChar = char;
+    }
+  }
+  return maxChar;
 };
 
 module.exports = maxCharacter;
--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -16,8 +16,41 @@
  * ]) === 'ABC, abc, CBA'
  */
 
-const stringifyId = (arr) => {
+// Brute Force
+// [1, 2, 3] => "1, 2, 3"
+// const stringifyId = (arr) => {
+//     const idArray = arr.map(obj => obj.id);
+//     const uniqueArray = [];
+//     let idString = "";
 
+//     for (let id of idArray) {
+//         if (!uniqueArray.includes(id)) {
+//             uniqueArray.push(id)
+//         }
+//     }
+
+//     for (let i = 0; i < uniqueArray.length; ++i) {
+//         if (i !== uniqueArray.length - 1) {
+//             idString += `${uniqueArray[i]}, `
+//         }
+//         else {
+//             idString += uniqueArray[i]
+//         }
+//     }
+
+//     return idString;
+// };
+
+// module.exports = stringifyId;
+
+
+
+// TOP TECH
+// [1, 2, 3] => "1, 2, 3"
+const stringifyId = (arr) => {
+  const idArray = arr.map((obj) => obj.id);
+  const uniqueArray = [...new Set(idArray)];
+  return uniqueArray.join(", ");
 };
 
 module.exports = stringifyId;
